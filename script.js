1d0fcd3e065dc2e2162324dca6aab4e835a40875
RiyanshLegend <160386630+RiyanshLegend@users.noreply.github.com>
2026-07-14 19:14:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-setTimeout(function () {
+const enter=document.getElementById("enterBtn");
 
-    document.getElementById("fireStatus").innerHTML =
-    "🔴 FIRE DETECTED";
-    document.getElementById("fireStatus").className = "red";
+const hero=document.querySelector(".hero");
 
-    document.getElementById("floodStatus").innerHTML =
-    "🔴 FLOOD DETECTED";
-    document.getElementById("floodStatus").className = "red";
+const dash=document.getElementById("dashboard");
 
-    document.getElementById("powerStatus").innerHTML =
-    "🟠 POWER FAILURE";
+enter.onclick=function(){
 
-    document.getElementById("waterStatus").innerHTML =
-    "🔴 WATER LEVEL HIGH";
+hero.style.display="none";
 
-}, 8000);
+dash.style.display="block";
+
+setTimeout(startEmergency,8000);
+
+}
+
+function startEmergency(){
+
+document.getElementById("fireStatus").innerHTML="🔴 FIRE DETECTED";
+
+document.getElementById("fireStatus").className="red";
+
+document.getElementById("floodStatus").innerHTML="🔴 FLOOD DETECTED";
+
+document.getElementById("floodStatus").className="red";
+
+document.getElementById("powerStatus").innerHTML="🟠 POWER FAILURE";
+
+document.getElementById("powerStatus").className="orange";
+
+document.getElementById("waterStatus").innerHTML="🔴 WATER LEVEL HIGH";
+
+document.getElementById("waterStatus").className="red";
+
+document.body.style.animation="flash .5s infinite";
+
+}
