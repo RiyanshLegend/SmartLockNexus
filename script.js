c4dcad98c434bdd3f4ef6558118aefe9198c44e6
RiyanshLegend <160386630+RiyanshLegend@users.noreply.github.com>
2026-07-14 20:04:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+setTimeout(function () {
 
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Smart AI City</title>
+    document.getElementById("fireStatus").innerHTML =
+    "🔴 FIRE DETECTED";
+    document.getElementById("fireStatus").className = "red";
 
-<link rel="stylesheet" href="style.css">
+    document.getElementById("floodStatus").innerHTML =
+    "🔴 FLOOD DETECTED";
+    document.getElementById("floodStatus").className = "red";
 
-</head>
+    document.getElementById("powerStatus").innerHTML =
+    "🟠 POWER FAILURE";
 
-<body>
+    document.getElementById("waterStatus").innerHTML =
+    "🔴 WATER LEVEL HIGH";
 
-<h1>🏙 SMART AI CITY</h1>
-
-<p class="subtitle">AI Powered City Dashboard</p>
-
-<div class="dashboard">
-
-<div class="card">
-<h2>🔥 Fire</h2>
-<p id="fireStatus" class="green">🟢 SAFE</p>
-</div>
-
-<div class="card">
-<h2>🌊 Flood</h2>
-<p id="floodStatus" class="green">🟢 SAFE</p>
-</div>
-
-<div class="card">
-<h2>⚡ Power</h2>
-<p id="powerStatus" class="green">🟢 NORMAL</p>
-</div>
-
-<div class="card">
-<h2>💧 Water</h2>
-<p id="waterStatus" class="green">🟢 NORMAL</p>
-</div>
-
-</div>
-
-<br><br>
-
-<button>🏠 HOUSE A</button>
-
-<script src="script.js"></script>
-
-</body>
-
-</html>
+}, 8000);
